f4b861bcde8a3b0fa487fccb48490a13ca4a4e05
n1mda <axel@appreviation.se>
2014-01-16 21:29:31 +0100

    Fixed typos

--- GIFConverter.h
+++ GIFConverter.h
@@ -35,10 +35,10 @@
 /**
  Convert a .GIF to .MP4 video file
  
- @param gif GIF image loaded into and NSData structure, eg.
+ @param gif GIF image loaded into an NSData structure, eg.
             [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://i.imgur.com/xqG0QP3.gif"]];
  @param speed Speed of output video, where 1.0 is actual speed, 2.0 is double speed
- @param size The wanted size of the output file, will keep aspect and fill rest with black. Use CGSizeMake(0.0,0.0) if you want to keep original size
+ @param size The wanted size of the output file, will keep aspect and fill rest with black. Use CGSizeMake(0, 0) if you want to keep original size
  @param output Path to output file, must not exist
  @param completion Block to call on completion, contains error if any
  */
